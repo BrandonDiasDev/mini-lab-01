e39836d77b924be3fe03aad90705f189319a86ed
Brandon Dias <dias.desenvolvimento@gmail.com>
2026-01-07 16:33:48 -0300

    Começando a ideia de API real

--- mini-lab-01/src/App.tsx
+++ mini-lab-01/src/App.tsx
@@ -1,41 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/monkey.jpeg.jfif'
-import viteLogo from './assets/monkey.jpeg.jfif'
-import './App.css'
+import { useEffect, useState } from "react";
+
+type User = {
+  id: number;
+  name: string;
+  email: string;
+};
 
 function App() {
-  
-  const [count, setCount] = useState(0)
+  const [users, setUsers] = useState<User[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((data) => {
+        setUsers(data);
+        setLoading(false);
+      });
+  }, []);
 
   return (
-    <>
-      <div>
+    <main>
+      <h1>Lista de usuários (API)</h1>
 
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+      {loading && <p>Carregando dados...</p>}
 
-      <h1>Trabalhar é pior que Morrer!</h1>
-      
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          Tomadas no cu em 2026 até o momento: {count}
-        </button>
-        <p>
-          Deus <code>socorro deus</code> porque deus ajuda deus
-        </p>
-      </div>
-
-      <p className="read-the-docs">
-        o q eu fiz deus
-      </p>
-
-    </>
-  )
+      {!loading && (
+        <ul>
+          {users.map((user) => (
+            <li key={user.id}>
+              {user.name} — {user.email}
+            </li>
+          ))}
+        </ul>
+      )}
+    </main>
+  );
 }
 
-export default App
+export default App;
